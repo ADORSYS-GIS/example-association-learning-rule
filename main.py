7640124367a99651d698ba2923e9a35ae0c1bb82
Elwizzy12 <chiahans123@gmail.com>
2023-12-09 10:16:16 +0100

    Implementation of apriori algorithm

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from src.apriori import apriori
 if __name__ == '__main__':
     result = apriori([[1, 2, 3], [1, 2, 3], [1, 2, 3]], 0.3, 0.7)
     print(result)
-#This my initial Comment
+
--- src/support.py
+++ src/support.py
@@ -1,9 +1,36 @@
-itemset_type = list[object]
-dataset_type = list[list[object]]
+from typing import List
 
+itemset_type = List[object]
+dataset_type = List[List[object]]
 
-def support(itemsets: dataset_type, data_set: itemset_type) -> float:
+def support(itemset: itemset_type, dataset: dataset_type) -> float:
     """
-    To find the frequency of itemsets in the dataset.
+    Calculate the support of an itemset in the dataset.
     """
-    pass
+    count = 0
+
+    for transaction in dataset:
+        if set(itemset).issubset(transaction):
+            count += 1
+
+    support_value = count / len(dataset)
+    return support_value
+
+# Example Usage:
+if __name__ == "__main__":
+    # Example dataset
+    dataset = [
+        ['apple', 'banana', 'cherry'],
+        ['banana', 'orange'],
+        ['apple', 'banana', 'cherry', 'orange'],
+        ['apple', 'cherry'],
+        ['banana', 'cherry'],
+    ]
+
+    # Example itemset
+    itemset = ['apple', 'banana']
+
+    # Calculate support
+    support_value = support(itemset, dataset)
+
+    print(f"The support of {itemset} in the dataset is: {support_value}")
