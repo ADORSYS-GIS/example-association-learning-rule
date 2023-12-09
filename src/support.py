e01d6e95fa56354e2f33dc5273deb211356e60ab
nkwajudetambe <nkwajudetambe@gmail.com>
2023-12-09 13:09:49 +0100

    removed file support.py(incorrect task executed on the file

--- src/support.py
+++
@@ -1,9 +0,0 @@
-itemset_type = list[object]
-dataset_type = list[list[object]]
-
-
-def support(itemsets: dataset_type, data_set: itemset_type) -> float:
-    """
-    To find the frequency of itemsets in the dataset.
-    """
-    pass
