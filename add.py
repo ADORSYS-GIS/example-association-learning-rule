cc9e52e846c0e83e4253191d01c4bc3b946cc479
nkwajudetambe <nkwajudetambe@gmail.com>
2023-12-09 09:03:40 +0100

    Deleted the file from the git repository

--- add.py
+++
@@ -1,4 +0,0 @@
-def add(a,b):
-    return a+b
-result = add(20,30)
-print(f"the reuslt of  numbers is : {result} ")
--- sub.py
+++
@@ -1,4 +0,0 @@
-def sub(a,b):
-    return a-b
-result = sub(20,30)
-print(f"the reuslt of  numbers is : {result} ")
