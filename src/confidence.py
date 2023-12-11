cd29615d618818f3aa8dce515ca740735bee9284
nkwajudetambe <nkwajudetambe@gmail.com>
2023-12-11 11:54:52 +0100

    feature(confidence.py):calculating the probabity of an itemset ocuuring and another occuring

--- src/confidence.py
+++ src/confidence.py
@@ -1,3 +1,4 @@
+from support import support
 dataset_type = list[list[object]]
 rule_type = tuple[list[object], list[object]]
 
@@ -6,4 +7,16 @@ def confidence(data_set: dataset_type, rule: rule_type) -> float:
     """
     To measure the likelihood of occurrence of an itemset given another itemset.
     """
-    pass
+    antecedent, consequent = rule
+
+    support_antecedent_consequent = support(data_set, antecedent + consequent)
+    support_antecedent = support(data_set, antecedent)
+
+    if support_antecedent != 0:
+        confidence_value = support_antecedent_consequent / support_antecedent
+    else:
+        confidence_value = 0
+
+    return confidence_value
+
+    
